f550e36013487b890cd52e6dd9bb9d7998f87938
hector <hectorugc4@gmail.com>
2020-10-02 14:58:10 +0200

    pas encore

--- tp_d'application/morpion.js
+++ tp_d'application/morpion.js
@@ -4,11 +4,11 @@ let jeur2 = prompt("Et toi quelle est ton prenom?");
 //Puis chosir la taille de morpion entre 3x3 et 8x8
 
 function creerLeMorpion() {
-	dimensionMoprion = prompt("Chosisie la taille de morpion");
+	dimensionMoprion = parseInt(prompt("Chosisie la taille de morpion"));
 	if (dimensionMoprion < 3 || dimensionMoprion > 8) {
 		while (dimensionMoprion < 3 || dimensionMoprion > 8) {
-			dimensionMoprion = prompt(
-				"Desole mais le morpion doit etre entre 3x3 et 8x8"
+			dimensionMoprion = parseInt(
+				prompt("Desole mais le morpion doit etre entre 3x3 et 8x8")
 			);
 		}
 	} else {
@@ -23,7 +23,7 @@ function creerLeMorpion() {
 		for (var i = 0; i < dimensionMoprion; i++) {
 			// creates a table row
 			var row = document.createElement("tr");
-
+			row.setAttribute("height", "50px");
 			for (var j = 0; j < dimensionMoprion; j++) {
 				// Create a <td> element and a text node, make the text
 				// node the contents of the <td>, and put the <td> at
@@ -44,12 +44,21 @@ function creerLeMorpion() {
 		tbl.appendChild(tblBody);
 		// appends <table> into <body>
 		body.appendChild(tbl);
-		// sets the border attribute of tbl to 2;
-		tbl.setAttribute("border", "2");
+		// sets the border attribute of tbl to 1;
+		tbl.setAttribute("border", "1");
+		tbl.setAttribute("height", "70%");
+		tbl.setAttribute("width", "100%");
 	}
 }
 creerLeMorpion();
+//On cree le symbole pour chaque joeur
+function dessinJoeur1() {
+	let symbole = "X";
+}
 //On gagne de la maniere traditional 3 symboles mais aussi on peut gagner
 //si n symbôles identiques doivent être alignés dans la grille nxn
+
+function validerMorpion() {}
+
 //en fin on demande s'ils veulent rejouer
 //On affiche en pied le score courant (nombre de parties gagnées par chacun).
